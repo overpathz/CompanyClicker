6a902d194866afb7bf022b1f32321398e5fc46d7
Serwio <kucelas@ukr.net>
2021-06-27 17:08:29 +0300

    -f

---
+++ src/main/java/com/CompanyClicker/Controllers/EnterController.java
@@ -0,0 +1,19 @@
+package com.CompanyClicker.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class EnterController {
+
+    @GetMapping("/")
+    public String getEnter(Principal principal) {
+        if (principal != null) {
+            return "redirect:/main";
+        }
+
+        return "index";
+    }
+}
---
+++ src/main/java/com/CompanyClicker/Entities/Player.java
@@ -0,0 +1,25 @@
+package com.CompanyClicker.Entities;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String login;
+    @NotNull
+    private String pass;
+    @NotNull
+    private Long score;
+}
