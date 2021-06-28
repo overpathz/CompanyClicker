1bb84090da9d8db954bc7b53a08cca6e7e111d26
Serwio <kucelas@ukr.net>
2021-06-28 13:03:00 +0300

    Added all html

---
+++ src/main/java/com/CompanyClicker/CompanyClickerApplication.java
@@ -0,0 +1,13 @@
+package com.CompanyClicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyClickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyClickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CompanyClicker/CompanyClickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CompanyClicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyClickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
